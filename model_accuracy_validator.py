4d61d4d5cf8f8731536d2ddd9fcb3b2289877c7b
Daniel <danielaugustoma@hotmail.com>
2022-11-17 17:21:12 -0600

    Avances realizados en metricas

--- model_accuracy_validator.py
+++ model_accuracy_validator.py
@@ -1,10 +1,11 @@
 import sys
 import pandas as pd
+from sklearn.preprocessing import label_binarize
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, f1_score, accuracy_score, classification_report, average_precision_score
 import pickle
 import matplotlib.pyplot as plt
-import seaborn as sn
+
 import time
 import csv
 import os
@@ -42,8 +43,9 @@ recall = recall_score(y, y_pred, average='macro')
 print("Recall: ", recall)
 f1 = f1_score(y, y_pred, average='macro')
 print("F1: ", f1)
-# avgPrecScore = average_precision_score(y, y_pred)
-# print(avgPrecScore)
+
+
+#acg precision sale de classification report
 
 con_disp = ConfusionMatrixDisplay(confusion_matrix= con, display_labels = clasesNoRepetidas)
 con_disp.plot()
@@ -52,30 +54,29 @@ plt.show()
 isFile1 = os.path.exists("resultadosRandomForest.csv")
 isFile2 = os.path.exists("resultadosDecisionTree.csv")
 headerDT = ["Train Dataset", "Criterion", "Depth", "Train Acc.", "Val Acc.", "Val Avg. Prec", "Val Avg. F1", "Time Train", "Time Val."]
-# headerRF = ["Train Dataset", "N Trees", "Depth", "Train Acc.", "Val Acc.", "Val Avg. Prec", "Val Avg. F1", "Time Train", "Time Val."]
-headerRF = ["Val Avg. Prec", "Val Avg. F1", "Time Train", "Train Acc"]
+headerRF = ["Train Dataset", "N Trees", "Depth", "Train Acc.", "Val Acc.", "Val Avg. Prec", "Val Avg. F1", "Time Train", "Time Val."]
 
-if(modelFile.__contains__("RandomForest")):
-    if(isFile1):
-        with open('resultadosRandomForest.csv', 'a') as file:
-            writer = csv.writer(file)
-            writer.writerow([recall, f1, pickle_model["trainingTime"], pickle_model["trainingAcc"]])
-    else:
-        # si no existe el archivo, lo creo y agrego una linea
-        with open('resultadosRandomForest.csv', 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(headerRF)
-            writer.writerow([recall, f1, pickle_model["trainingTime"], pickle_model["trainingAcc"]])
-            print("Datos escritos en resultadosRandomForest.csv satisfactoriamente")
-else:
-    if(isFile2):
-        print("Existe el archivo")
-        with open('resultadosDecisionTree.csv', 'a') as file:
-            writer = csv.writer(file)
-            writer.writerow([modelFile])
-    else:
-        with open('resultadosDecisionTree.csv', 'w' ) as file:
-            writer = csv.writer(file)
-            writer.writerow(headerDT)
-            writer.writerow([modelFile])
+# if(modelFile.__contains__("RandomForest")):
+#     if(isFile1):
+#         with open('resultadosRandomForest.csv', 'a') as file:
+#             writer = csv.writer(file)
+#             writer.writerow([recall, f1, pickle_model["trainingTime"], pickle_model["trainingAcc"]])
+#     else:
+#         # si no existe el archivo, lo creo y agrego una linea
+#         with open('resultadosRandomForest.csv', 'w') as file:
+#             writer = csv.writer(file)
+#             writer.writerow(headerRF)
+#             writer.writerow([recall, f1, pickle_model["trainingTime"], pickle_model["trainingAcc"]])
+#             print("Datos escritos en resultadosRandomForest.csv satisfactoriamente")
+# else:
+#     if(isFile2):
+#         print("Existe el archivo")
+#         with open('resultadosDecisionTree.csv', 'a') as file:
+#             writer = csv.writer(file)
+#             writer.writerow([modelFile])
+#     else:
+#         with open('resultadosDecisionTree.csv', 'w' ) as file:
+#             writer = csv.writer(file)
+#             writer.writerow(headerDT)
+#             writer.writerow([modelFile])
     
