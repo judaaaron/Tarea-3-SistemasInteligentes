4ef2d140c247065a7380d544f0f3d44725bf44fb
judaaaron <judaponce14@unitec.edu>
2022-11-16 19:55:23 -0600

    Trees and Forest entrenados con todas las combinaciones
    Co-authored-by: D11110 <D11110@users.noreply.github.com>

--- decision_tree.py
+++ decision_tree.py
@@ -4,35 +4,31 @@ import sys
 import pickle
 
 #receive the first, second, third and fourth argument in command line and save them in variables
-nombrearchivoentrenar = sys.argv[1]
-nombrearchivovalidacion = sys.argv[2]
-criterio = sys.argv[3]
+trainFile = sys.argv[1]
+criterio = sys.argv[2]
 try:
-    depth = sys.argv[4]
+    depth = sys.argv[3]
 except IndexError:
     depth = None
 
+preName = trainFile.split(".")[1]
+preName = preName.split("_")[2]
 
-#validation data
-validation_data = pd.read_csv(nombrearchivovalidacion)
-clasesValidaciones = validation_data.pop("class")
-validation_data = validation_data.replace({"Si": 1, "No": 0})
-colsVal = validation_data.columns.tolist()
-xVal = validation_data[colsVal]
-
-#training data
-training_data = pd.read_csv(nombrearchivoentrenar)
+training_data = pd.read_csv(trainFile)
 clases = training_data.pop('class')
 training_data = training_data.replace({"Si": 1, "No": 0})
 cols = training_data.columns.tolist()
 x = training_data[cols]
 y = clases
 
-
-clf = tree.DecisionTreeClassifier(criterion=criterio, max_depth=None)
+if depth is not None:
+    depth = int(depth)
+clf = tree.DecisionTreeClassifier(criterion=criterio, max_depth=depth)
 clf = clf.fit(x,y)
-nombre = "DecisionTree-"+ criterio + "-" + depth + ".pkl"
-print(nombre)
+nombre = "./Modelos/DecisionTree/DecisionTree-" + preName + "-"+ criterio + "-" + str(depth) + ".pkl"
 filename = nombre
 with open(filename, 'wb') as f:
     pickle.dump(clf, f)
+
+
+print("Modelo guardado en:", nombre, "con criterio =", criterio, "y max_depth =", str(depth))
--- random_forest.py
+++ random_forest.py
@@ -5,32 +5,30 @@ from sklearn.metrics import classification_report
 import pickle
 
 
-archivosTraining = ["training_data_small.csv", "training_data_medium.csv", "training_data_large.csv", "training_data_very_large.csv"]
-nPosibles = [5, 10, 50, 100]
-depths = [2, 4, 6]
+trainFile = sys.argv[1]
+n = sys.argv[2]
+depth = sys.argv[3]
 
 #for each archivosTraining, for each nPosibles, for each depth, create a model and save it in a file
 
-for trainFile in archivosTraining:
-    for n in nPosibles:
-        for depth in depths:
-            i = 1
-            preName = trainFile.split(".")[0]
-            preName = preName.split("_")[2]
-            
-            training_data = pd.read_csv(trainFile)
-            clases = training_data.pop('class')
-            training_data = training_data.replace({"Si": 1, "No": 0})
-            cols = training_data.columns.tolist()
-            x = training_data[cols]
-            y = clases
-            
-            rfc = RandomForestClassifier(n_estimators=n, max_depth=depth)
-            rfc.fit(x,y)
-            nombre = "./modelsRFC/RandomForest-" + preName + "-" + str(n) + "-" + str(depth) + ".pkl"
-            filename = nombre
-            with open(filename, 'wb') as f:
-                pickle.dump(rfc, f)
+#print(trainFile, n, depth)
+preName = trainFile.split(".")[1]
+preName = preName.split("_")[2]
+
+training_data = pd.read_csv(trainFile)
+clases = training_data.pop('class')
+training_data = training_data.replace({"Si": 1, "No": 0})
+cols = training_data.columns.tolist()
+x = training_data[cols]
+y = clases
+
+rfc = RandomForestClassifier(n_estimators=int(n), max_depth=int(depth))
+rfc.fit(x,y)
+nombre = "./Modelos/RandomForest/RandomForest-" + preName + "-" + str(n) + "-" + str(depth) + ".pkl"
+with open(nombre, 'wb') as f:
+    pickle.dump(rfc, f)
+
+print("Modelo guardado en:", nombre, "con n_estimators = ", n, "y max_depth = ", depth)
             
 
 
