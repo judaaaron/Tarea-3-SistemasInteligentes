1b93cfec5eac25a33217cedb97bfa36174966d96
judaaaron <judaponce14@unitec.edu>
2022-11-17 00:42:56 -0600

    metricas escritas en xsv Co-authored-by: D11110 <D11110@users.noreply.github.com>

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,9 @@
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import sys
 import pickle
+import time
 
 #receive the first, second, third and fourth argument in command line and save them in variables
 trainFile = sys.argv[1]
@@ -14,6 +16,7 @@ except IndexError:
 preName = trainFile.split(".")[1]
 preName = preName.split("_")[2]
 
+start = time.time()
 training_data = pd.read_csv(trainFile)
 clases = training_data.pop('class')
 training_data = training_data.replace({"Si": 1, "No": 0})
@@ -21,14 +24,27 @@ cols = training_data.columns.tolist()
 x = training_data[cols]
 y = clases
 
+
+
+
 if depth is not None:
     depth = int(depth)
-clf = tree.DecisionTreeClassifier(criterion=criterio, max_depth=depth)
+clf = DecisionTreeClassifier(criterion=criterio, max_depth=depth)
 clf = clf.fit(x,y)
+
+end = time.time()
+print("Tiempo total de entrenamiento Decision Tree: ", end-start)  
+
+y_pred = clf.predict(x)
+val = accuracy_score(y_true=y, y_pred=y_pred)
+
+datos = {"trainingTime": end-start, "trainingAcc": val, "modelo": clf}
+print("Datos de entrenamiento: ",datos)
+
 nombre = "./Modelos/DecisionTree/DecisionTree-" + preName + "-"+ criterio + "-" + str(depth) + ".pkl"
 filename = nombre
 with open(filename, 'wb') as f:
-    pickle.dump(clf, f)
+    pickle.dump(datos, f)
 
 
-print("Modelo guardado en:", nombre, "con criterio =", criterio, "y max_depth =", str(depth))
+print("Modelo guardado en:", nombre,)
--- random_forest.py
+++ random_forest.py
@@ -1,9 +1,9 @@
 import sys
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 import pickle
-
+import time
 
 trainFile = sys.argv[1]
 n = sys.argv[2]
@@ -15,6 +15,7 @@ depth = sys.argv[3]
 preName = trainFile.split(".")[1]
 preName = preName.split("_")[2]
 
+start = time.time()
 training_data = pd.read_csv(trainFile)
 clases = training_data.pop('class')
 training_data = training_data.replace({"Si": 1, "No": 0})
@@ -24,11 +25,18 @@ y = clases
 
 rfc = RandomForestClassifier(n_estimators=int(n), max_depth=int(depth))
 rfc.fit(x,y)
+
+end = time.time()
+print("Tiempo total de entrenamiento Random Forest: ", end-start) 
+
+y_pred = rfc.predict(x)
+val = accuracy_score(y_true=y, y_pred=y_pred)
+
+datos = {"trainingTime": end-start, "trainingAcc": val, "modelo": rfc}
+print("Datos de entrenamiento: ",datos)
+
 nombre = "./Modelos/RandomForest/RandomForest-" + preName + "-" + str(n) + "-" + str(depth) + ".pkl"
 with open(nombre, 'wb') as f:
-    pickle.dump(rfc, f)
+    pickle.dump(datos, f)
 
-print("Modelo guardado en:", nombre, "con n_estimators = ", n, "y max_depth = ", depth)
-            
-
-
+print("Modelo guardado en:", nombre)
